3251674b9bcb08b9298f0421156d6b8c888df2c6
Nick Cox <xoc.kcin@gmail.com>
2022-03-01 19:11:46 -0500

    refactored

--- befunge.js
+++ befunge.js
@@ -2,9 +2,9 @@ const testString = ">987v>.v\nv456<  :\n>321 ^ _@";
 
 function interpret(code) {
   // step one: turn string into 2d array
-  const lines = code.split("\n")
-  const grid = lines.map(line => line.split(""))
-  
+  const grid = code.split("\n").map(line => line.split(""))
+
+
 }
 
 interpret(testString)
