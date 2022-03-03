454d9c3bb307ac0b97d0935cf54249ede808294f
Nick Cox <xoc.kcin@gmail.com>
2022-03-02 19:09:08 -0500

    define state

--- befunge.js
+++ befunge.js
@@ -1,3 +1,10 @@
+/* 
+note: At every step, I need to know two things:
+1. What do I do?
+2. Which direction am I going?
+
+*/
+
 const testString = ">987v>.v\nv456<  :\n>321 ^ _@";
 
 function interpret(code) {
@@ -6,8 +13,17 @@ function interpret(code) {
 
   // step two: define stack
   const stack = []
+  const output = []
 
+  // step three: define state
+  const state = {
+    location: [0,0],
+    direction: grid[0][0]
+  }
   
+
+  // 
+
 }
 
 interpret(testString)
