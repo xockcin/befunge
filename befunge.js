b0263bf8eea01582f1e0896ea73ae44c80d78119
Nick Cox <xoc.kcin@gmail.com>
2022-03-03 08:37:43 -0500

    doesn't work yet but progress!

--- befunge.js
+++ befunge.js
@@ -10,7 +10,7 @@ const testString = ">987v>.v\nv456<  :\n>321 ^ _@";
 function interpret(code) {
   // step one: turn string into 2d array
   const grid = code.split("\n").map(line => line.split(""))
-
+  console.log(grid)
   // step two: define stack
   const stack = []
   const output = []
@@ -144,7 +144,7 @@ function interpret(code) {
       return output
     } else {
       doSymbol(symbol)
-      doMove(direction)
+      doMove(state.direction)
     }
   }
 
