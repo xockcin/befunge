27106004f25e9864d7b21d76d4d7cc635a3b4c56
Nick Cox <xoc.kcin@gmail.com>
2022-03-02 19:51:36 -0500

    symbols

--- befunge.js
+++ befunge.js
@@ -17,12 +17,102 @@ function interpret(code) {
 
   // step three: define state
   const state = {
-    location: [0,0],
-    direction: grid[0][0]
+    x: 0,
+    y: 0,
+    direction: ">"
   }
   
-
-  // 
+  // step four: define symbols
+  function doSymbol(symbol) {
+    switch (symbol) {
+      case "+":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(a + b);
+        break;
+      case "-":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(b - a);
+        break;
+      case "*":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(a * b);
+        break;
+      case "/":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(b / a);
+        break;
+      case "%":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(b % a);
+        break;
+      case "!":
+        stack.push(+!!stack.pop());
+        break
+      case "`":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(b > a);
+        break
+      case ">":
+        state.direction = ">";
+        break
+      case "<":
+        state.direction = "<";
+        break
+      case "^":
+        state.direction = "^";
+        break
+      case "v":
+        state.direction = "v";
+        break
+      case "?":
+        state.direction = [">", "<", "^", "v"][Math.floor(Math.random() * 4)];
+        break
+      case "_":
+        const x = stack.pop();
+        state.direction = x ? (state.direction = "<") : (state.direction = ">");
+        break
+      case "|":
+        const x = stack.pop();
+        state.direction = x ? (state.direction = "^") : (state.direction = "v");
+        break
+      case "\"":
+        // TODO
+      case ":":
+        stack.push(stack[stack.length ? stack.length - 1 : 0])
+        break
+      case "\\":
+        const a = stack.pop();
+        const b = stack.pop();
+        stack.push(b)
+        stack.push(a)
+        break
+      case "$":
+        stack.push()
+        break
+      case ".":
+        const x = stack.pop()
+        output.push(x)
+        break
+      case ",":
+        const x = stack.pop()
+        output.push()
+        break
+      case "p":
+        // TODO
+      case "g":
+        // TODO
+      case "@":
+        // TODO
+      case " ":
+        break
+    }
+  }
 
 }
 
